38b7110794422f3bcf2d77d4822d2b104e91d571
Jihwan Kim <mongtons990213@gmail.com>
2022-07-13 18:37:35 +0900

    Upload Spring Boot Framework Files and directory

---
+++ src/test/java/com/example/RaOn_Community/RaOnCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RaOn_Community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaOnCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
