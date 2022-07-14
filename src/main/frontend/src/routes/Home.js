6a779310bf894feac648e2f9e8056d8047ba8b8e
euncheol0206 <euncheol0206@gmail.com>
2022-07-14 15:59:05 +0900

    update App.js, Home.js

--- src/main/frontend/src/routes/Home.js
+++ src/main/frontend/src/routes/Home.js
@@ -1,12 +1,10 @@
 import { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
-import Board from "../components/Board";
 
 function Home() {
   return (
     <>
       <NavBar />
-      <Board />
     </>
   );
 }
