d4b0bf12b020014e6a811e4170840f3c0589f6b3
euncheol0206 <euncheol0206@gmail.com>
2022-07-14 19:28:08 +0900

    connect react and spring boot

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,13 +1,28 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Home from "./routes/Home";
 import Department from "./routes/Department";
 import Major from "./routes/Major";
+import Board from "./routes/Board";
 
 function App() {
+  const [hello, setHello] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((response) => setHello(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(hello);
   return (
     <Router>
       <Switch>
+        <Route path="/board">
+          <Board />
+        </Route>
         <Route path="/major">
           <Major />
         </Route>
