f6344fb698762ca65ca0a41e52e2b88add763a40
mongtons <mongtons990213@gmail.com>
2022-07-16 18:22:55 +0900

    Add three GetMappings Annotation

--- src/main/java/com/example/RaOn_Community/controller/mainPageController.java
+++ src/main/java/com/example/RaOn_Community/controller/mainPageController.java
@@ -9,4 +9,16 @@ public class mainPageController {
     public String index(){
         return "main";
     }
+    @GetMapping("/api/소프트웨어학부")
+    public String soft_Notice(){
+        return "";
+    }
+    @GetMapping("/api/인공지능융합학부")
+    public String AI_Notice(){
+        return "";
+    }
+    @GetMapping("/api/데이터사이언스학부")
+    public String Data_Notice(){
+        return "";
+    }
 }
